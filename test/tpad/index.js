cdf6870b7bdfb5b1d05877464dcb0b6ac2c5ffd3
NISAL <535964903@qq.com>
2021-04-20 17:43:58 +0800

    update test case

--- test/tpad/index.js
+++ test/tpad/index.js
@@ -8,8 +8,8 @@ axios.defaults.headers['Authorization'] = `Basic ${TOKEN}`;
 
 axios.post('https://api.tapd.cn/bugs', {
   title: 'api添加的缺陷 带图片测试2',
-  workspace_id: '42607816',
-  reporter: 'DongJianYe',
+  workspace_id: '',
+  reporter: '',
   description: '<p>1233</p><img src="https://ncstatic.clewm.net/rsrc/2021/0418/21/dd7e23f66b939cac6c7d118fd0c20681.jpg" />'
 }).then((res) => {
   console.log(res);
